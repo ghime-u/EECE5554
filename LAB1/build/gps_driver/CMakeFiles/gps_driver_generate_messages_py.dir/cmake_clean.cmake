060825c7ed37e635e7a903012c0fff169edf5f9c
root <root@Singularity>
2023-02-06 19:26:40 -0500

    LAB1 commit4

---
+++ LAB1/build/gps_driver/CMakeFiles/gps_driver_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/root/catkin_ws/devel/lib/python3/dist-packages/gps_driver/msg/__init__.py"
+  "/root/catkin_ws/devel/lib/python3/dist-packages/gps_driver/msg/_gps_msg.py"
+  "CMakeFiles/gps_driver_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gps_driver_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAB1/build/gps_driver/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/catkin_ws/src/gps_driver
+# Build directory: /root/catkin_ws/build/gps_driver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
